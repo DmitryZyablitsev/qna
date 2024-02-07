934c265a843f56997c2d24dcb3208536b29a09ac
DmitryZyablitsev <dima.zyablicev@mail.ru>
2024-02-07 10:34:15 +0500

    rubocop-refactoring-2

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-feature 'User can sign in', %q{
+feature 'User can sign in', "
   In order to ask questions
   As an unauthenticated user
   I'd like to be able to sign in
-} do
-
+" do
   given(:user) { create(:user) }
 
   background { visit new_user_session_path }
@@ -16,6 +15,7 @@ feature 'User can sign in', %q{
 
     expect(page).to have_content 'Signed in successfully.'
   end
+
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
