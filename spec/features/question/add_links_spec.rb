ae8d1bc96862d52a32e5959845a929d6f1d461a1
DmitryZyablitsev <dima.zyablicev@mail.ru>
2024-03-23 17:10:32 +0500

    add multiple links to a question or answer

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -8,7 +8,7 @@ I'd like to be able to add links
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/DmitryZyablitsev/e07cf54bb8c3eacd8e7be06d7eeeacb0' }
 
-  scenario 'User adds link when asks question' do
+  scenario 'User adds several links when asking a question' do
     sign_in(user)
     visit new_question_path
 
@@ -17,9 +17,14 @@ I'd like to be able to add links
 
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
+    click_on 'add link'
+
+    fill_in 'Link name', with: 'My gist2'
+    fill_in 'Url', with: gist_url
 
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'My gist2', href: gist_url
   end
 end
