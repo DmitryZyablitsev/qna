27f0f4dbe7b5caa48c867aafaf8e2761a8911e0e
DmitryZyablitsev <dima.zyablicev@mail.ru>
2024-03-30 10:38:56 +0500

    написан тест автор добавляет ссылки при редактировании вопроса

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -6,6 +6,7 @@ As an question's author
 I'd like to be able to add links
 " do
   given(:user) { create(:user) }
+  given(:question) { create(:question, author: user) }
   given(:gist_url) { 'https://gist.github.com/DmitryZyablitsev/11b2834129c6e9897f680ae4fd6c59d8' }
   given(:school_url) { 'https://thinknetica.com' }
 
@@ -47,10 +48,12 @@ I'd like to be able to add links
   describe 'Author adds links when edit question', :js do
     background do
       sign_in(user)
-      visit new_question_path
+      visit question_path(question)
 
-      click_on 'Edit question'
-      click_on 'add link'
+      within '.question' do
+        click_on 'Edit question'
+        click_on 'add link'
+      end
     end
 
     scenario 'with valid url' do
@@ -58,11 +61,14 @@ I'd like to be able to add links
         fill_in 'Link name', with: 'My link'
         fill_in 'Url', with: school_url
         click_on 'add link'
-        within '.nested-fields' do
+
+        within all('.nested-fields').last do
           fill_in 'Link name', with: 'My gist'
           fill_in 'Url', with: gist_url
         end
+
         click_on 'Save'
+
         within '.links' do
           expect(page).to have_link 'My link', href: school_url
           expect(page).to have_content 'Hello World'
@@ -70,7 +76,23 @@ I'd like to be able to add links
       end      
     end
 
-    scenario 'with invalid url'
+    scenario 'with invalid url' do
+      within '.question' do
+        fill_in 'Link name', with: 'My link'
+        fill_in 'Url', with: "invalid url"
+        click_on 'add link'
 
+        within all('.nested-fields').last do
+          fill_in 'Link name', with: 'My gist'
+          fill_in 'Url', with: "invalid url_2"
+        end
+
+        click_on 'Save'
+        
+        expect(page).to have_content 'Links url is invalid'
+        expect(page).to have_no_link 'My link'
+        expect(page).to have_no_link 'My gist'
+      end
+    end
   end
 end
