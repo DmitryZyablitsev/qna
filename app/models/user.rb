b63c4c6ced5f28c68530bba70a9df61adeb771e4
DmitryZyablitsev <dima.zyablicev@mail.ru>
2024-05-15 22:04:40 +0500

    full-scrincast

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,24 +11,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    return authorization.user if authorization
-
-    email = auth.info[:email]
-    user = User.where(email: email).first
-    unless user
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-    end
-    user.create_authorization(auth)
-    user
+    FindForOauth.new(auth).call
   end
 
   def author_of?(resource)
     resource.author_id == id
-  end
-
-  def create_authorization(auth)
-    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
