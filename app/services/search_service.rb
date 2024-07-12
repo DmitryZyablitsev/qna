6d2a8ed2688eb402a51249328a53c558ed25b0f8
DmitryZyablitsev <dima.zyablicev@mail.ru>
2024-07-12 10:07:36 +0500

    fix

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -5,7 +5,7 @@ class SearchService
 
   def self.call(search_params)
     if MODELS_THAT_SUPPORT_SEARCH.include? search_params[:area]
-      search_result = Object.const_get(search_params[:area]).search "#{search_params[:query]}"   
+      search_result = search_params[:area].safe_constantize.search "#{search_params[:query]}"   
     elsif search_params[:area] == SEARCH_IN_ALL_MODELS
       search_result = ThinkingSphinx.search "#{search_params[:query]}"
     else
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
+  describe 'GET #search' do
 
+    it 'global search' do
+      expect(ThinkingSphinx).to receive(:search).with 'test'
+      get :search, params: {'query' => 'test', 'area' => 'Global', 'commit' => 'Search'} 
+    end
+
+    ["Question", "Answer", "Comment", "User" ].each do |scope|
+      it "search in scopes" do
+        expect(scope.constantize).to receive(:search).with 'test'
+        get :search, params: {'query' => 'test', 'area' => scope, 'commit' => 'Search'}
+      end
+    end
+
+    it 'params invalid, assigns [] to @search_result' do
+      get :search, params: {'query' => 'test', 'area' => 'invalid', 'commit' => 'Search'}
+      expect(assigns(:search_result)).to eq []
+    end
+  end
 end
