8b260ad2d259ae10382549e6d747aff984057e42
DmitryZyablitsev <dima.zyablicev@mail.ru>
2024-07-31 12:43:23 +0500

    fix-unocorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,12 +7,6 @@ set :branch, 'main'
 set :deploy_to, "/home/deployer/qna"
 set :deploy_user, 'deployer'
 
-# set :rbenv_ruby, '3.2.3'
-
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-# set :rbenv_roles, :all # default value
-
 set :bundle_jobs, 1
 
 append :linked_files, "config/database.yml", 'config/master.key', 'config/thinking_sphinx.yml'
